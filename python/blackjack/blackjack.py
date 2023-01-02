ad14abfc069477c64ad4a490a622da0f196e0449
Joshua Childers <58181922+joshchilders@users.noreply.github.com>
2023-01-01 19:14:11 -0500

    Add files via upload

--- python/blackjack/blackjack.py
+++ python/blackjack/blackjack.py
@@ -28,81 +28,103 @@ def main():
     # Deal a new hand
     # deal_hand(deck, hand, dealerHand)
 
-    # Deal initial hands
-    deal = True
-    while deal:
-        deal_card(deck, playerHand)
-        deal_card(deck, dealerHand)
-        if len(dealerHand) > 1:
-            deal = False
+    # Keep dealing new hands until player exits
+    play = True
+    while play:
 
-    # Print cards to terminal
-    print()
-    print_hand(playerHand)
-    print()
-    first = True
-    for card in dealerHand:
-        if first:
-            first = False
-            print("| \u00B7 |", end="")
-        else:
-            print(f"{card}", end="")
-    print()
-
-    # Ask player for new card
-    bust = False
-    while True:
-        print()
-        userInput = input("Hit? ").lower()
-        if userInput == "n":
-            break
-        elif userInput == "y":
+        # Deal initial hands
+        deal = True
+        while deal:
             deal_card(deck, playerHand)
-            print()
-            print_hand(playerHand)
-            print()
-            first = True
-            for card in dealerHand:
-                if first:
-                    first = False
-                    print("| \u00B7 |", end="")
-                else:
-                    print(f"{card}", end="")
-            print()
-            if calculate_hand_value(playerHand) > 21:
-                bust = True
-                print()
-                print("==== B U S T ====")
-                print()
-                break
+            deal_card(deck, dealerHand)
+            if len(dealerHand) > 1:
+                deal = False
 
-    if bust == False:
+        # Print cards to terminal
         print()
         print_hand(playerHand)
-        print_hand(dealerHand)
-        while calculate_hand_value(dealerHand) < 16:
-            time.sleep(2)
-            deal_card(deck, dealerHand)
+        print()
+        first = True
+        for card in dealerHand:
+            if first:
+                first = False
+                print("| \u00B7 |", end="")
+            else:
+                print(f"{card}", end="")
+        print()
+
+        # Ask player for new card
+        bust = False
+        while True:
+            print()
+            userInput = input("Hit? ").lower()
+            if userInput == "n":
+                break
+            elif userInput == "y":
+                deal_card(deck, playerHand)
+                print()
+                print_hand(playerHand)
+                print()
+                first = True
+                for card in dealerHand:
+                    if first:
+                        first = False
+                        print("| \u00B7 |", end="")
+                    else:
+                        print(f"{card}", end="")
+                print()
+                if calculate_hand_value(playerHand) > 21:
+                    bust = True
+                    print()
+                    print("==== BUST ====")
+                    print()
+                    break
+
+        if bust == False:
             print()
             print_hand(playerHand)
             print_hand(dealerHand)
-            time.sleep(1)
-        if calculate_hand_value(dealerHand) > 21:
-            print()
-            print("=== DEALER BUST ===")
-            print()
-        elif calculate_hand_value(dealerHand) > calculate_hand_value(playerHand):
-            print()
-            print("=== DEALER WINS ===")
-            print()
-        elif calculate_hand_value(dealerHand) < calculate_hand_value(playerHand):
-            print()
-            print("=== YOU WIN ===")
-            print()
-        elif calculate_hand_value(dealerHand) == calculate_hand_value(playerHand):
-            print()
-            print("=== IT'S A TIE ===")
-            print()
+            while calculate_hand_value(dealerHand) < 16:
+                time.sleep(2)
+                deal_card(deck, dealerHand)
+                print()
+                print_hand(playerHand)
+                print_hand(dealerHand)
+                time.sleep(1)
+            if calculate_hand_value(dealerHand) > 21:
+                print()
+                print("=== DEALER BUST ===")
+                print()
+            elif calculate_hand_value(dealerHand) > calculate_hand_value(playerHand):
+                print()
+                print("=== DEALER WINS ===")
+                print()
+            elif calculate_hand_value(dealerHand) < calculate_hand_value(playerHand):
+                print()
+                print("=== YOU WIN ===")
+                print()
+            elif calculate_hand_value(dealerHand) == calculate_hand_value(playerHand):
+                print()
+                print("=== IT'S A TIE ===")
+                print()
+        
+        # Shuffle hands back into deck
+        for card in playerHand:
+            deck.append(card)
+        for card in dealerHand:
+            deck.append(card)
+
+        playerHand.clear()
+        dealerHand.clear()
+
+        random.shuffle(deck)
+        
+        # Ask if player wants to keep playing
+        userInput = input("New hand? ").lower()
+        if userInput == "y":
+            play = True
+        else:
+            play = False
 
 
 def create_suits(deck, suit):
@@ -110,7 +132,7 @@ def create_suits(deck, suit):
     for i in range(1, 14):
         if i == 1:
             face = "A"
-            value = 1
+            value = 11
         elif i == 10:
             face = 0
             value = 10
@@ -154,6 +176,13 @@ def calculate_hand_value(hand):
     value = 0
     for card in hand:
         value += card.value
+    
+    # Handle aces (1 or 11)
+    if value > 21:
+        for card in hand:
+            if card.face == "A":
+                value -= 10
+
     return value
 
 
